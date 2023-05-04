ba808847ccac852447088b3d666db502a2c88f68
painter98 <120320713+painter98@users.noreply.github.com>
2023-05-04 19:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
-
+let prices = document.querySelectorAll('.price');
+let sum = 0;
+console.log(prices)
 const getSum = () => {
 //Add your code here
-  
+  prices.forEach((cost)=>{
+	  sum+=parseInt(cost.innerText);
+  });
+
+	let tr = document.createElement('tr');
+	let td = document.createElement('td');
+	td.innerHTML = `${sum}`;
+	tr.appendChild(td);
+	
+	document.querySelector('.tabled').appendChild(tr);
+//console.log(sum);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
